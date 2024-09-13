2f59ae3750cb1ab63e374b20832c8d1c3d6b602d
ArielRodriguez07 <a.22308051280706@cbtis128.edu.mx>
2024-09-13 09:52:42 -0600

    Add files via upload

--- funcionesv2.py
+++ funcionesv2.py
@@ -3,6 +3,8 @@ print("ariel rodriguez")
 #zona de listas tuplas sets y diccionarios
 celulares=["samsung","iphone 11","chafa"]
 bandas=["enjambre","zoe","porter"]
+canciones=["bajo la lluvia","azul","murcielago"]
+computadoras=["hp","mac","microsoft"]
 #zona de funciones
 def verlista(telefonos):
     for uncelular in telefonos:
@@ -10,12 +12,22 @@ def verlista(telefonos):
 def verbanda(rock):
     for unabanda in rock:
         print(unabanda)
+def vercanciones(songs):
+    for unacancion in songs:
+        print(unacancion)
+def vercomputadoras(marca):
+    for unacompu in marca:
+        print(unacompu)
 
 #llamada de funciones
 print("imprime celulares de una lista ")
 verlista(celulares)
 print("imprime una lista de bandas de rock")
 verbanda(bandas)
+print("imprime una lista de canciones de rock")
+verbanda(canciones)
+print("imprime una lista de computadoras")
+verbanda(computadoras)
 print("Esto es un ejemplo de diccionario")
 ## diccionario
 audifonos = {
